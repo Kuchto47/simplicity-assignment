a3f067fd77e86703aad3c66940c6484ddb1c94b9
Kuchto47 <martin.k738@gmail.com>
2025-12-30 13:25:36 +0100

    refactor category dto

--- simplicity-backend/src/modules/announcements/model/category.dto.ts
+++ simplicity-backend/src/modules/announcements/model/category.dto.ts
@@ -2,17 +2,13 @@ import { CategoryDtoType } from './category.dto.schema';
 import { Category } from './category.entity';
 
 export class CategoryDto implements CategoryDtoType {
-  createdAt: string;
   id: string;
   name: string;
-  updatedAt: string;
 
   static fromEntity(entity: Category): CategoryDto {
     return {
-      createdAt: entity.createdAt.toISOString(),
       id: entity.id,
       name: entity.name,
-      updatedAt: entity.updatedAt.toISOString(),
     };
   }
 }
