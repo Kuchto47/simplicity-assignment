3fd88f0859e44a6026db347c37bbd09f8738fd27
Kuchto47 <martin.k738@gmail.com>
2025-12-30 21:23:29 +0100

    fix wiring of tRPC client<>server

--- simplicity-frontend/src/trpc/trpc.ts
+++ simplicity-frontend/src/trpc/trpc.ts
@@ -1,10 +1,10 @@
-import { createTRPCClient, httpBatchLink } from '@trpc/client';
+import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '../../../simplicity-backend/src/trpc/@generated/server.ts';
 
 // after installing @trpc/server, for some reason, AppRouter is erroring as a type T_T
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-expect-error
-export const trpcClient = createTRPCClient<AppRouter>({
+// Unfortunately, because of nestjs-trpc and trpc-panel in the backend,
+// which are outdated and only support tRPC v10, I had to downgrade tRPC from v11 to v10 both in frontend and backend
+export const trpcClient = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
       url: 'http://localhost:3030/trpc', // TODO ENV
