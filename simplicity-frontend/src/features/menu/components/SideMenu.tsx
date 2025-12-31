b2c80cea535a7a3a73e0e737c8d2b36d7dd0dee4
Kuchto47 <martin.k738@gmail.com>
2025-12-31 10:32:49 +0100

    refactor navbar and sidemenu

---
+++ simplicity-frontend/src/features/menu/components/SideMenu.tsx
@@ -0,0 +1,28 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet.tsx';
+import { Menu } from 'lucide-react';
+
+export const SideMenu = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="cursor-pointer" size="30" />
+      </SheetTrigger>
+      <SheetContent side="left">
+        <SheetHeader>
+          <SheetTitle>Are you absolutely sure?</SheetTitle>
+          <SheetDescription>
+            This action cannot be undone. This will permanently delete your
+            account and remove your data from our servers.
+          </SheetDescription>
+        </SheetHeader>
+      </SheetContent>
+    </Sheet>
+  );
+};
