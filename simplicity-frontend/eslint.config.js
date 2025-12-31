5b6781eb4736149da16c526c7360bcc6d48fe67b
Kuchto47 <martin.k738@gmail.com>
2025-12-31 13:13:54 +0100

    enhance eslint rule for _

--- simplicity-frontend/eslint.config.js
+++ simplicity-frontend/eslint.config.js
@@ -22,6 +22,16 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
   },
   eslintPluginPrettierRecommended,
 ]);
