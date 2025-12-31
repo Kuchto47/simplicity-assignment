1336f20e7b9fc1e57a4011b4020103021b7e3340
Kuchto47 <martin.k738@gmail.com>
2025-12-31 11:41:12 +0100

    add client styles to SideMenu

--- simplicity-frontend/src/features/menu/components/SideMenu.tsx
+++ simplicity-frontend/src/features/menu/components/SideMenu.tsx
@@ -7,19 +7,25 @@ import {
   SheetTrigger,
 } from '@/components/ui/sheet.tsx';
 import { Menu, LucideHome, MegaphoneIcon } from 'lucide-react';
-import { Link } from '@tanstack/react-router';
+import { Link, useMatchRoute } from '@tanstack/react-router';
 
 export const SideMenu = () => {
+  const matchRoute = useMatchRoute();
+  const isAnnouncementsActive = matchRoute({
+    to: '/announcements',
+    fuzzy: true,
+  });
+
   return (
     <Sheet>
       <SheetTrigger>
         <Menu className="cursor-pointer" size="30" />
       </SheetTrigger>
       <SheetContent side="left" className="p-8">
-        <SheetHeader className="p-0">
+        <SheetHeader className="p-0 mb-4">
           <Link to="/">
-            <SheetClose className="cursor-pointer">
-              <SheetTitle className="flex gap-2">
+            <SheetClose className="cursor-pointer w-full">
+              <SheetTitle className="flex gap-2 hover:bg-primary/30 px-1 h-10 items-center">
                 <LucideHome size="24" />
                 Test City
               </SheetTitle>
@@ -27,8 +33,10 @@ export const SideMenu = () => {
           </Link>
         </SheetHeader>
         <Link to="/announcements">
-          <SheetClose className="cursor-pointer">
-            <div className="pt-4 flex gap-2">
+          <SheetClose className="cursor-pointer w-full">
+            <div
+              className={`flex h-10 px-1 gap-2 items-center hover:bg-primary/30 ${isAnnouncementsActive ? 'bg-primary/40' : ''}`}
+            >
               <MegaphoneIcon />
               Announcements
             </div>
