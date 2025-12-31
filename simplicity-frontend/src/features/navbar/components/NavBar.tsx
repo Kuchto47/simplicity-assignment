a92ea9c8b4a47f0e40df2a77dc436b9f585670ed
Kuchto47 <martin.k738@gmail.com>
2025-12-31 11:42:54 +0100

    delete commented code

--- simplicity-frontend/src/features/navbar/components/NavBar.tsx
+++ simplicity-frontend/src/features/navbar/components/NavBar.tsx
@@ -1,4 +1,3 @@
-//import { Link } from '@tanstack/react-router';
 import { SideMenu } from '@/features/menu/components/SideMenu.tsx';
 
 export const NavBar = () => {
@@ -7,12 +6,6 @@ export const NavBar = () => {
       <header className="px-8 py-4 flex h-20 justify-center">
         <div className="flex gap-6 items-center w-full">
           <SideMenu />
-          {/*<Link to="/" className="[&.active]:font-bold">*/}
-          {/*  Home*/}
-          {/*</Link>{' '}*/}
-          {/*<Link to="/about" className="[&.active]:font-bold">*/}
-          {/*  About*/}
-          {/*</Link>*/}
         </div>
       </header>
       <hr />
