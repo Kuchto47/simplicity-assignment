3c4b6e7c04a65e0e98e3c5804c65c75f108542b9
Kuchto47 <martin.k738@gmail.com>
2026-02-08 12:43:25 +0100

    remove trpc server from dev deps, not needed

--- simplicity-frontend/src/trpc/trpc.ts
+++ simplicity-frontend/src/trpc/trpc.ts
@@ -1,7 +1,6 @@
 import { createTRPCClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from '../../../simplicity-backend/src/trpc/@generated/server.ts';
 
-// after installing @trpc/server, for some reason, AppRouter is erroring as a type T_T
 export const trpcClient = createTRPCClient<AppRouter>({
   links: [
     httpBatchLink({
